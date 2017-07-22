4e6fe1334e11ee8a364f63d4dc760120b3fb4111
Dominic Motuka <dominic.motuka@gmail.com>
2017-07-22 12:39:02 +0300

    Draw a green dot with paper js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
 
-    // TODO
+    var c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+
     paper.view.draw();
     console.log('main.js loaded');
 });
